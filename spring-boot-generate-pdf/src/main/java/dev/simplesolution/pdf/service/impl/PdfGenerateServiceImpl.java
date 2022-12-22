93610bec0e8daf97e2d5923138f30555d730e106
Deepak Lodha <deepak.lodha@nexturn.com>
2022-12-22 17:39:49 +0530

    pdf generate and send email

---
+++ spring-boot-generate-pdf/src/main/java/dev/simplesolution/pdf/service/impl/PdfGenerateServiceImpl.java
@@ -0,0 +1,45 @@
+package dev.simplesolution.pdf.service.impl;
+
+import com.lowagie.text.DocumentException;
+import dev.simplesolution.pdf.service.PdfGenerateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Map;
+
+@Service
+public class PdfGenerateServiceImpl implements PdfGenerateService {
+    private Logger logger = LoggerFactory.getLogger(PdfGenerateServiceImpl.class);
+    private static final String PDF_OUTPUT = "src/main/resources/test1pdf.pdf";
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void generatePdfFile(String templateName, Map<String, Object> data, String pdfFileName) {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String htmlContent = templateEngine.process(templateName, context);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("Offer Letter2.pdf"));
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(fileOutputStream, false);
+            renderer.finishPDF();
+        } catch (FileNotFoundException e) {
+            logger.error(e.getMessage(), e);
+        } catch (DocumentException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
